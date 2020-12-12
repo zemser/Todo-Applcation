515884643eebb762a7566db93ba386f6f4ff5fb7
Izzy Zemser <zemser@gmail.com>
2020-12-12 16:48:00 +0200

    first

---
+++ resful-web-service/src/main/java/com/rest/resfulwebservice/todo/Todo.java
@@ -0,0 +1,90 @@
+package com.rest.resfulwebservice.todo;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Todo {
+    private long id;
+    private String username;
+    private String description;
+    private Date targetDate;
+    private boolean isDone;
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", description='" + description + '\'' +
+                ", targetDate=" + targetDate +
+                ", isDone=" + isDone +
+                '}';
+    }
+
+    public Todo() {
+
+    }
+
+    public Todo(long id, String username, String description, Date targetDate, boolean isDone) {
+        this.id = id;
+        this.username = username;
+        this.description = description;
+        this.targetDate = targetDate;
+        this.isDone = isDone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getTargetDte() {
+        return targetDate;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTargetDte(Date targetDate) {
+        this.targetDate = targetDate;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Todo todo = (Todo) o;
+        return id == todo.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ resful-web-service/src/test/java/com/rest/resfulwebservice/ResfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.resfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
